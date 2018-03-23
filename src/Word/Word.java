d41e2a8d3cfe1ae7adc83d17c92bf11ccd69ae9f
WATARU\鳴海航 <wataruwelt@gnail.com>
2018-03-23 18:30:25 +0900

    ホームワークJAVA０１

---
+++ src/Word/Word.java
@@ -0,0 +1,30 @@
+package Word;
+import colLesson01.colLesson01;
+
+public class Word {
+    private String word;
+    private String meaning;
+
+    public Word(String word, String meaning) {
+        this.word = word;
+        this.meaning = meaning;
+
+//        public void attack () {
+//            System.out.println("単語：" + word + "意味:" + meaning);
+//        }
+//        public String getWord(){
+//            return word;
+//        }
+//        public void setWord(String word){
+//            this.word = word;
+//        }
+//
+//        public String getMeaning() {
+//            return meaning;
+//        }
+//        public void setWaza(String meaning) {
+//            this.meaning = meaning;
+//        }
+//    }
+    }
+}
---
+++ src/colLesson01/colLesson01.java
@@ -0,0 +1,34 @@
+package colLesson01;
+
+public class colLesson01 {
+    public static void main(String[] args) {
+
+
+     String word ;
+     String mean;
+     int j;
+        String word1 = new java.util.Scanner(System.in).nextLine();
+        String word2 = new java.util.Scanner(System.in).nextLine();
+        String word3 = new java.util.Scanner(System.in).nextLine();
+        String word4 = new java.util.Scanner(System.in).nextLine();
+        String word5 = new java.util.Scanner(System.in).nextLine();
+        String word6 = new java.util.Scanner(System.in).nextLine();
+        String word7 = new java.util.Scanner(System.in).nextLine();
+        String word8 = new java.util.Scanner(System.in).nextLine();
+        String word9 = new java.util.Scanner(System.in).nextLine();
+        String word10 = new java.util.Scanner(System.in).nextLine();
+//
+//        System.out.println("次の単語と意味を入力して下さい。\"e\"で終了します。");
+
+        while (word != "e") {
+            System.out.println("次の単語と意味を入力して下さい。\"e\"で終了します。");
+        }
+
+
+                    for (int i =0; i<4 ; i++){
+                        System.out.println("単語：" + "" + "意味" + "");
+}
+                         System.out.println(4+"件登録しました。");
+    }
+
+    }
